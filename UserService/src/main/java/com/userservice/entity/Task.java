850572c93d8bfeae2bf289c7284906be444ffc6f
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-13 18:56:31 -0400

    Add Task entity

---
+++ UserService/src/main/java/com/userservice/entity/Task.java
@@ -0,0 +1,82 @@
+package com.userservice.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @ManyToOne
+    private User assignee;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private Date deadline;
+
+    @ManyToOne
+    private Project project;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(User assignee) {
+        this.assignee = assignee;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+}
