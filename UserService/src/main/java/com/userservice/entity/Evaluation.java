f125444ceb64eaea8096d8162aeb4cf7d52a6579
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-22 11:29:18 -0400

    Refactored Evaluation entity to use Lombok
    - Added Lombok annotations @Data and @NoArgsConstructor.
    - Removed manual getters and setters.

--- UserService/src/main/java/com/userservice/entity/Evaluation.java
+++ UserService/src/main/java/com/userservice/entity/Evaluation.java
@@ -1,7 +1,12 @@
 package com.userservice.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Evaluation {
     @Id
@@ -19,53 +24,4 @@ public class Evaluation {
 
     @ManyToOne
     private Project project;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public User getEvaluator() {
-        return evaluator;
-    }
-
-    public void setEvaluator(User evaluator) {
-        this.evaluator = evaluator;
-    }
-
-    public User getEvaluatee() {
-        return evaluatee;
-    }
-
-    public void setEvaluatee(User evaluatee) {
-        this.evaluatee = evaluatee;
-    }
-
-    public Project getProject() {
-        return project;
-    }
-
-    public void setProject(Project project) {
-        this.project = project;
-    }
 }
