de9372162c79ccb11bd79099c45e6ee4a4903af4
BAKOMEN TATTO PAUL CHRISTIAN <bakomen.tatto_paul_christian@courrier.uqam.ca>
2024-06-22 10:01:28 -0400

    Implemente les endpoints

--- src/main/java/com/copartage/authservice/controller/AuthController.java
+++ src/main/java/com/copartage/authservice/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.copartage.authservice.controller;
 
+import com.copartage.authservice.util.TokenBlacklist;
 import com.copartage.authservice.model.User;
 import com.copartage.authservice.service.UserService;
+import com.copartage.authservice.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,12 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private TokenBlacklist tokenBlacklist;
+
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody User user) {
         User savedUser = userService.saveUser(user);
@@ -23,10 +31,16 @@ public class AuthController {
     public ResponseEntity<?> login(@RequestBody User user) {
         User existingUser = userService.findByUsername(user.getUsername());
         if (existingUser != null && userService.checkPassword(user.getPassword(), existingUser.getPassword())) {
-            // Generate a JWT token here and return it
-            return ResponseEntity.ok("Login successful");
+            String token = jwtUtil.generateToken(user.getUsername());
+            return ResponseEntity.ok(token);
         } else {
             return ResponseEntity.status(401).body("Invalid username or password");
         }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestHeader(name = "Authorization") String token) {
+        tokenBlacklist.addToken(token);
+        return ResponseEntity.ok("Logout successful");
     }
 }
