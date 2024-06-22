e5c6e0c27ac55d1ebfd7a162d2b47f232f1e7499
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-22 11:36:45 -0400

    Refactored User entity to use Lombok and improved JPA annotations
    - Added Lombok annotations @Data and @NoArgsConstructor.
    - Removed manual getters and setters.
    - Changed the projects relationship to @OneToMany(mappedBy = "creator") to correctly map the relationship.

--- UserService/src/main/java/com/userservice/entity/User.java
+++ UserService/src/main/java/com/userservice/entity/User.java
@@ -1,8 +1,13 @@
 package com.userservice.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class User {
     @Id
@@ -16,7 +21,7 @@ public class User {
     @OneToOne(cascade = CascadeType.ALL)
     private Profile profile;
 
-    @ManyToMany
+    @OneToMany(mappedBy = "creator")
     private Set<Project> projects;
 
     @ManyToMany
@@ -33,93 +38,4 @@ public class User {
 
     @ManyToMany
     private Set<Tag> tags;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Profile getProfile() {
-        return profile;
-    }
-
-    public void setProfile(Profile profile) {
-        this.profile = profile;
-    }
-
-    public Set<Project> getProjects() {
-        return projects;
-    }
-
-    public void setProjects(Set<Project> projects) {
-        this.projects = projects;
-    }
-
-    public Set<UserGroup> getGroups() {
-        return groups;
-    }
-
-    public void setGroups(Set<UserGroup> groups) {
-        this.groups = groups;
-    }
-
-    public Set<Notification> getNotifications() {
-        return notifications;
-    }
-
-    public void setNotifications(Set<Notification> notifications) {
-        this.notifications = notifications;
-    }
-
-    public Set<Evaluation> getEvaluationsReceived() {
-        return evaluationsReceived;
-    }
-
-    public void setEvaluationsReceived(Set<Evaluation> evaluationsReceived) {
-        this.evaluationsReceived = evaluationsReceived;
-    }
-
-    public Set<Evaluation> getEvaluationsGiven() {
-        return evaluationsGiven;
-    }
-
-    public void setEvaluationsGiven(Set<Evaluation> evaluationsGiven) {
-        this.evaluationsGiven = evaluationsGiven;
-    }
-
-    public Set<Tag> getTags() {
-        return tags;
-    }
-
-    public void setTags(Set<Tag> tags) {
-        this.tags = tags;
-    }
 }
