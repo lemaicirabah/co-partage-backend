6480fff51f9bf43e741a5d3354abf0941c05496c
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-22 11:37:24 -0400

    Refactored UserGroup entity to use Lombok and improved JPA annotations
    - Added Lombok annotations @Data.
    - Removed manual getters and setters.
    - Added @JoinColumn annotation to the owner field.
    - Changed members relationship to use @JoinTable for join columns.
    - Changed projects relationship to @OneToMany(mappedBy = "group") to correctly map the relationship.

--- UserService/src/main/java/com/userservice/entity/UserGroup.java
+++ UserService/src/main/java/com/userservice/entity/UserGroup.java
@@ -1,72 +1,30 @@
 package com.userservice.entity;
 
+import lombok.Data;
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@Data
 public class UserGroup {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String description;
 
     @ManyToOne
+    @JoinColumn(name = "owner_id")
     private User owner;
 
     @ManyToMany
+    @JoinTable(
+            name = "user_group_members",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
     private Set<User> members;
 
-    @ManyToMany
+    @OneToMany(mappedBy = "group")
     private Set<Project> projects;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public User getOwner() {
-        return owner;
-    }
-
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
-
-    public Set<User> getMembers() {
-        return members;
-    }
-
-    public void setMembers(Set<User> members) {
-        this.members = members;
-    }
-
-    public Set<Project> getProjects() {
-        return projects;
-    }
-
-    public void setProjects(Set<Project> projects) {
-        this.projects = projects;
-    }
 }
