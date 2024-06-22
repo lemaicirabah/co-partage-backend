8d7f82f32be4e823dba905abc9f157ea550fe45c
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-22 11:32:29 -0400

    Refactored Project entity to use Lombok
    - Added Lombok annotations @Data.
    - Removed manual getters and setters.
    - Updated ManyToOne, ManyToMany, and OneToMany relationships.
    - Added group field with ManyToOne relationship.

--- UserService/src/main/java/com/userservice/entity/Project.java
+++ UserService/src/main/java/com/userservice/entity/Project.java
@@ -1,21 +1,28 @@
 package com.userservice.entity;
 
+import lombok.Data;
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@Data
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
     private String description;
 
     @ManyToOne
+    @JoinColumn(name = "creator_id")
     private User creator;
 
     @ManyToMany
+    @JoinTable(
+            name = "project_members",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
     private Set<User> members;
 
     @OneToMany(mappedBy = "project")
@@ -24,60 +31,7 @@ public class Project {
     @OneToMany(mappedBy = "project")
     private Set<Evaluation> evaluations;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public User getCreator() {
-        return creator;
-    }
-
-    public void setCreator(User creator) {
-        this.creator = creator;
-    }
-
-    public Set<User> getMembers() {
-        return members;
-    }
-
-    public void setMembers(Set<User> members) {
-        this.members = members;
-    }
-
-    public Set<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(Set<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    public Set<Evaluation> getEvaluations() {
-        return evaluations;
-    }
-
-    public void setEvaluations(Set<Evaluation> evaluations) {
-        this.evaluations = evaluations;
-    }
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private UserGroup group;
 }
