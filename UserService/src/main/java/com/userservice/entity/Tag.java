fa187d24b780811c00dc25e1b07c16e7238b14ba
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-22 11:34:42 -0400

    Refactored Tag entity to use Lombok
    - Added Lombok annotations @Data and @NoArgsConstructor.
    - Removed manual getters and setters.

--- UserService/src/main/java/com/userservice/entity/Tag.java
+++ UserService/src/main/java/com/userservice/entity/Tag.java
@@ -1,10 +1,15 @@
 package com.userservice.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Tag {
     @Id
@@ -12,21 +17,4 @@ public class Tag {
     private Long id;
 
     private String name;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
