9ccff737026a254305e359bc2502cfc2d76347c8
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-22 11:33:53 -0400

    Refactored Skill entity to use Lombok
    - Added Lombok annotations @Data and @NoArgsConstructor.
    - Removed manual getters and setters.

--- UserService/src/main/java/com/userservice/entity/Skill.java
+++ UserService/src/main/java/com/userservice/entity/Skill.java
@@ -1,10 +1,15 @@
 package com.userservice.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Skill {
     @Id
@@ -13,29 +18,4 @@ public class Skill {
 
     private String name;
     private String description;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
