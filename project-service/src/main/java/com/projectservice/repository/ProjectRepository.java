c89d91b860b887119f6bc2571c4821c9a6e2a665
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-26 14:46:05 -0400

    Delete ProjectService module

--- project-service/src/main/java/com/projectservice/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projectservice.repository;
-
-import com.projectservice.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
-
--- project-service/src/main/java/com/projectservice/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projectservice.repository;
-
-import com.projectservice.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
