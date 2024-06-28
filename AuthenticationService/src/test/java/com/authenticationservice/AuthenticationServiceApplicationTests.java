26141eaf5b58279b839e74570b209c5fe4c2449e
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-28 15:26:14 -0400

    Set CI/CD with mock databases

--- AuthenticationService/src/test/java/com/authenticationservice/AuthenticationServiceApplicationTests.java
+++ AuthenticationService/src/test/java/com/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.authenticationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("ci")
 public class AuthenticationServiceApplicationTests {
 
 	@Test
