0e415a3f2864900b930c8452d8d043aed1aa737e
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-06-29 19:42:36 -0400

    Add OpenApiConfig for AthenticationService

---
+++ AuthenticationService/src/main/java/com/authenticationservice/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.authenticationservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Authentication Service API")
+                        .version("1.0")
+                        .description("API documentation for the Authentication Service"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("authentication-service")
+                .pathsToMatch("/authentication/**")
+                .build();
+    }
+}
