ce6c12f2d35ea31e6041d11333ffdb3b4919a7e8
lemaicirabah <lemaicirabah@gmail.com>
2024-07-05 01:22:02 -0400

    Add EvaluationRepository interface

---
+++ EvaluationService/src/main/java/com/evaluationservice/repository/EvaluationRepository.java
@@ -0,0 +1,10 @@
+package com.evaluationservice.repository;
+
+import com.evaluationservice.entity.Evaluation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+}
+
