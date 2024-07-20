886c9096e41ef8bc27f1c91d7ca7594a784bcf9f
Frédéric Grenier <jj591929@ens.uqam.ca>
2024-07-20 16:32:40 -0400

    Ajout lombrok project service

--- ProjectService/src/main/java/com/projectservice/dto/ProjectDto.java
+++ ProjectService/src/main/java/com/projectservice/dto/ProjectDto.java
@@ -1,76 +1,26 @@
 package com.projectservice.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Set;
 
+@Data
+@NoArgsConstructor
 public class ProjectDto {
+    // Getter et Setter pour id
     private Long id;
+    // Getter et Setter pour title
     private String title;
+    // Getter et Setter pour description
     private String description;
+    // Getter et Setter pour creator
     private Long creator;
+    // Getter et Setter pour members
     private Set<Long> members;
+    // Getter et Setter pour tasks
     private Set<TaskDto> tasks;
+    // Getter et Setter pour evaluations
     private Set<Long> evaluations;
 
-    // Getter et Setter pour id
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    // Getter et Setter pour title
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    // Getter et Setter pour description
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    // Getter et Setter pour creator
-    public Long getCreator() {
-        return creator;
-    }
-
-    public void setCreator(Long creator) {
-        this.creator = creator;
-    }
-
-    // Getter et Setter pour members
-    public Set<Long> getMembers() {
-        return members;
-    }
-
-    public void setMembers(Set<Long> members) {
-        this.members = members;
-    }
-
-    // Getter et Setter pour tasks
-    public Set<TaskDto> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(Set<TaskDto> tasks) {
-        this.tasks = tasks;
-    }
-
-    // Getter et Setter pour evaluations
-    public Set<Long> getEvaluations() {
-        return evaluations;
-    }
-
-    public void setEvaluations(Set<Long> evaluations) {
-        this.evaluations = evaluations;
-    }
 }
