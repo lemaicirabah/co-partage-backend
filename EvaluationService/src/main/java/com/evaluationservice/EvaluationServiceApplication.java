e8e68d2fe7146f95a61d261cba3038f5d4167bbc
Frédéric Grenier <jj591929@ens.uqam.ca>
2024-07-20 14:53:23 -0400

    correctif

--- EvaluationService/src/main/java/com/evaluationservice/EvaluationServiceApplication.java
+++ EvaluationService/src/main/java/com/evaluationservice/EvaluationServiceApplication.java
@@ -2,8 +2,10 @@ package com.evaluationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // Ajoutez cette annotation
 public class EvaluationServiceApplication {
 
     public static void main(String[] args) {
